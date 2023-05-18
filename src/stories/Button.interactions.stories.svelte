a948b416267db32f32b1d49576b8a498e209ff2a
Stefan Ilie P5 <stefan.ilie@ipsos.com>
2023-05-18 11:40:51 +0300

    first commit

---
+++ src/stories/Button.interactions.stories.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+	import { Meta, Story, Template } from '@storybook/addon-svelte-csf';
+	import Button from './Button.svelte.js';
+	import { within, waitFor, userEvent } from '@storybook/testing-library';
+
+	import { expect } from '@storybook/jest';
+</script>
+
+<Meta
+	title="Example/Button"
+	component={Button}
+	parameters={{
+		docs: {}
+	}}
+	argTypes={{
+		backgroundColor: { control: 'color' },
+		size: {
+			control: { type: 'select' },
+			options: ['small', 'medium', 'large']
+		}
+	}}
+/>
+
+<Template let:args>
+	<Button {...args} />
+</Template>
+
+<Story
+	name="Primary"
+	args={{
+		primary: true,
+		label: 'Button'
+	}}
+	play={({ canvasElement }) => {
+		userEvent.tab({ focusTrap: canvasElement });
+	}}
+/>
---
+++ src/stories/Button.stories.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+	import { Meta, Story, Template } from '@storybook/addon-svelte-csf';
+	import Button from './Button.svelte.js';
+</script>
+
+<Meta
+	title="Example/Button"
+	component={Button}
+	parameters={{
+		docs: {}
+	}}
+	argTypes={{
+		backgroundColor: { control: 'color' },
+		size: {
+			control: { type: 'select' },
+			options: ['small', 'medium', 'large']
+		}
+	}}
+/>
+
+<Template let:args>
+	<Button {...args} />
+</Template>
+
+<Story
+	name="Primary"
+	args={{
+		primary: true,
+		label: 'Button'
+	}}
+/>
